7fe2c3d96276d3a920f53d9d2159293f4546cc96
Jacob Baldwin <baldwinj30@gmail.com>
2024-12-26 23:09:46 -0500

    simple query of cfbd api

---
+++ README.md
@@ -0,0 +1,3 @@
+# CFB Ratings
+
+Install `uv`, set up your cfbd api key in your .env file and run `uv run cfb-ratings`.
--- src/cfb_ratings/__init__.py
+++ src/cfb_ratings/__init__.py
@@ -1,2 +1,14 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 def main() -> None:
-    print("Hello from cfb-ratings!")
+    cfbd_api_key = os.getenv("CFBD_API_KEY")
+    if cfbd_api_key is None:
+        print("Set your api key in a .env file")
+        return
+    response = requests.get(url="https://api.collegefootballdata.com/games?year=2022", headers={"Authorization": f"Bearer {cfbd_api_key}"})
+    print(response.json())
