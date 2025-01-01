989af7c56b024293708a25f810d759fd58e69d81
Jacob Baldwin <baldwinj30@gmail.com>
2025-01-01 17:22:25 -0500

    load the game-level data into a dataframe

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CFB Ratings
 
 Install `uv`, set up your cfbd api key in your .env file and run `uv run cfb-ratings`.
+
+
+## Dev Setup
+
+To add pre-commit hooks; `uv run pre-commit install`.
--- src/cfb_ratings/__init__.py
+++ src/cfb_ratings/__init__.py
@@ -1,14 +1,40 @@
+"""Experimental script."""
+
+import os
+
+import polars as pl
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 
 def main() -> None:
+    """Test functionality."""
     cfbd_api_key = os.getenv("CFBD_API_KEY")
     if cfbd_api_key is None:
         print("Set your api key in a .env file")
         return
-    response = requests.get(url="https://api.collegefootballdata.com/games?year=2022", headers={"Authorization": f"Bearer {cfbd_api_key}"})
-    print(response.json())
+    response = requests.get(
+        url="https://api.collegefootballdata.com/games?year=2014",
+        headers={"Authorization": f"Bearer {cfbd_api_key}"},
+    )
+    df = pl.from_dicts(
+        response.json(),
+        schema={
+            "id": pl.Int64,
+            "season": pl.Int64,
+            "week": pl.Int64,
+            "home_id": pl.Int64,
+            "home_team": pl.String,
+            "home_conference": pl.String,
+            "home_division": pl.String,
+            "home_points": pl.Int64,
+            "away_id": pl.Int64,
+            "away_team": pl.String,
+            "away_conference": pl.String,
+            "away_division": pl.String,
+            "away_points": pl.Int64,
+        },
+    )
+    print(df)
